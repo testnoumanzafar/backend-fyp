6bf93be88f9733ed1c6fba89688d43d3a63528c4
testnoumanzafar <mtj3310@gmail.com>
2025-08-12 23:42:51 -0700

    trydeploy

--- index.js
+++ index.js
@@ -13,18 +13,36 @@ const groupRouter = require('./routes/groupRoutes');
 const Group = require('./models/group');
 
 const app = express();
+
+app.use(cors({
+  origin: process.env.FRONTEND_URL,
+  methods: ["GET", "POST"],
+  credentials: true
+}));
+
+
 const server = http.createServer(app);
 // process.env.FRONTEND_URL || '*'
+// const io = new Server(server, {
+//   cors: {
+//     origin: process.env.FRONTEND_URL || '*',
+//     methods: ['GET', 'POST'],
+//   },
+// });
+   
 const io = new Server(server, {
   cors: {
-    origin: process.env.FRONTEND_URL || '*',
-    methods: ['GET', 'POST'],
+    origin: process.env.FRONTEND_URL,
+    methods: ["GET", "POST"],
   },
 });
 
+
 app.set('io', io);  
 
-app.use(cors());
+// app.use(cors());
+
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
